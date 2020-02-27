22933343a1a6a664e5ce043cae98ccbd5304bd1c
Ted Balmer <54968949+tedatmatc@users.noreply.github.com>
2020-02-27 14:21:38 -0600

    Completed Activity 5

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -4,7 +4,7 @@ require('dotenv').config({
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Blog`,
+    title: `Ted's Blog`,
     description: `ITDEV-164 Gatsby Blog.`,
     author: `Ted Balmer`,
     contact: {
